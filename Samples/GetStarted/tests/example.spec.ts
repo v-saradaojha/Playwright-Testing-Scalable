7fbf68f39bf53d7dc724569487b8d4435dc793ee
Sarada Ojha <v-saradaojha@microsoft.com>
2024-10-17 14:58:21 +0530

    run scalable test updated

--- Samples/GetStarted/tests/example.spec.ts
+++ Samples/GetStarted/tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { runSimplePageTest } from "../util";
 
 for (let i = 0; i < 200; i++) {
-  test(`Run scalable Test`, async () => {
+  test(`Run scalable Test ${i}`, async () => {
     let serviceRunId = process.en.PLAYWRIGHT_SERVICE_RUN_ID;
     const wsEndpoint = `${process.env.PLAYWRIGHT_SERVICE_URL}?cap={"os":"${'linux'}","runId":"${serviceRunId}"}`;
     const _browser = await playwright["chromium"].connect(wsEndpoint, { headers: { 'x-mpt-access-key': process.env.PLAYWRIGHT_SERVICE_ACCESS_TOKEN } });
